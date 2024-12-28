ca8b7695cfaa00c5c28e2eedab14f501ccc96197
Anton Trofimuk <anton.trofimuk.1@gmail.com>
2024-12-28 14:54:13 +0300

    all tasks resolved

--- homework/tasks/task_7.py
+++ homework/tasks/task_7.py
@@ -1,5 +1,6 @@
 import abc
 import asyncio
+from concurrent.futures import ThreadPoolExecutor
 
 
 class AbstractModel:
@@ -19,4 +20,7 @@ class Handler:
         # Тест проверяет, что время исполнения одной корутины handle_request не слишком сильно
         # отличается от времени исполнения нескольких таких корутин, запущенных конкурентно.
         #
-        # YOU CODE GOES HERE
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            loop = asyncio.get_event_loop()
+            await loop.run_in_executor(executor, self._model.compute)
+
